6c1c7e84047a09d4dc3d94f9b822dd54ab51ee47
Lee Morgan <me@leemorgan.io>
2024-01-20 10:53:29 -0500

    Add flag icon. Add universal css file.

--- routes.js
+++ routes.js
@@ -2,5 +2,10 @@ const {Eta} = require("eta");
 let eta = new Eta({views: `${__dirname}/views`});
 
 module.exports = (app)=>{
+    let views = `${__dirname}/views/`;
+
+    //IMAGES
+    app.get("/img/flag", (req, res)=>res.sendFile(`${views}/img/flag.png`));
+
     app.get("/", (req, res)=>res.send(eta.render("landing.eta")));
 }
