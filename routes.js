b50ff358a8addfc6be3f9ce7844a1b551e1b0a4e
Lee Morgan <me@leemorgan.io>
2024-02-17 11:28:20 -0500

    Start the verbs page, realize I have no idea how to do it, quit. Add words template so that I can just add words easily.

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ module.exports = (app)=>{
     //IMAGES
     app.get("/img/flag", (req, res)=>res.sendFile(`${views}/img/flag.png`));
 
+    //WORDS
+    app.get('/words/:word', (req, res)=>res.send(eta.render(`words/word.eta`, {word: req.params.word})));
+
     //OTHER
     app.get("/", (req, res)=>res.send(eta.render("landing.eta")));
     app.get("/food", (req, res)=>res.send(eta.render("food.eta")));
