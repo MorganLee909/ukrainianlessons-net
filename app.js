17884afe6bcca62ddd56bd7eb222f4c9ac828d72
Lee Morgan <me@leemorgan.io>
2024-02-18 15:02:11 -0500

    Remove flashcards stuff since it will be on a subdomain.

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const compression = require("compression");
-const mongoose = require("mongoose");
 
 let mongooseOptions = {};
 
-let mongoString = "mongodb://127.0.0.1/flashcards";
-
 let app = express();
-
-if(process.env.NODE_ENV == "production"){
-    mongoString = `mongodb://website:${process.env.MONGODB_PASS}@127.0.0.1:27017/flashcards?authSource=admin`;
-}
-
-mongoose.connect(mongoString, mongooseOptions);
 
 app.use(compression());
 
